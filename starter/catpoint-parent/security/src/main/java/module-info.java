3e35495cce1e864cd882adf985de7492c7e38540
Tungha113 <haxuantung09t1@gmail.com>
2024-12-11 06:31:03 +0700

    Update code

--- starter/catpoint-parent/security/src/main/java/module-info.java
+++ starter/catpoint-parent/security/src/main/java/module-info.java
@@ -5,8 +5,5 @@ module com.udacity.catpoint.security {
     requires com.udacity.catpoint.image;
     requires java.desktop;
     requires java.prefs;
-    exports com.udacity.catpoint.security.service;
-    exports com.udacity.catpoint.security.data;
-    exports com.udacity.catpoint.security.application;
     opens com.udacity.catpoint.security.data to com.google.gson;
 }
